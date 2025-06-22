b6fbc8b6817746a5df724e8303389b9f65f6228f
Jorge Jimenez <jorgejimenez@mail.weber.edu>
2025-06-22 10:58:14 -0600

    fix include path issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,17 @@
-#include "imgui.h" // necessary for ImGui::*, imgui-SFML.h doesn't include imgui.h
-#include "imgui-SFML.h" // for ImGui::SFML::* functions and SFML-specific overloads
 #include <SFML/Graphics.hpp>
+#include "Game.h"
 #include "shapes/Shape.h"
+#include "vec2/Vec2.h"
 
 #include <iostream>
-
-static void showWindowWithMenu()
-{
-    ImGui::Begin("Window");
-        if (ImGui::BeginMenuBar())
-        {
-            if (ImGui::BeginMenu("File"))
-            {
-                ImGui::MenuItem("Open..", "Ctrl+O");
-                ImGui::MenuItem("Save", "Ctrl+S");
-                ImGui::MenuItem("Close", "Ctrl+W");
-                ImGui::EndMenu();
-            }
-            ImGui::EndMenuBar();
-        }
-
-        ImGui::Text("Hello, world!");
-    ImGui::End();
-}
-
-static void showWindowWithcCheckbox(const char* label, bool* v)
-{
-    ImGui::Begin("Checkbox");
-        ImGui::LabelText("label", "%s", label);
-        ImGui::Checkbox(label, v);
-    ImGui::End();
-}
+#include <iomanip>
+#include <string>
+#include <functional>
+#include <vector>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({1140, 680}), "ImGui + SFML = <3");
-    window.setFramerateLimit(60);
-
-    if (!ImGui::SFML::Init(window)) {
-        std::cerr << "ImGui SFML initialization failed" << std::endl;
-        return 1;
-    }
-    
-    // Use our new Shape class instead of sf::CircleShape directly
-    cmakelearn::Shape shape;
-    shape.setPosition(570.f, 340.f); // center in window
-    static bool check = true;
-
-    sf::Clock deltaClock;
-    while (window.isOpen())
-    {
-        while (const auto event = window.pollEvent()) {
-            ImGui::SFML::ProcessEvent(window, *event);
-
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            }
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-        showWindowWithMenu();
-        showWindowWithcCheckbox("Checkbox", &check);
-        window.clear();
-        if (check)
-        {
-            shape.setFillColor(sf::Color::Green);
-        }
-        else
-        {
-            shape.setFillColor(sf::Color::Red);
-        }
-        
-        // Use our draw method instead
-        shape.draw(window);
-        ImGui::SFML::Render(window);
-        window.display();
-    }
-
-    ImGui::SFML::Shutdown();
+    Game game("config.json");
+    game.run();
     return 0;
 }
--- src/vec2/Vec2.h
+++ src/vec2/Vec2.h
@@ -2,15 +2,93 @@
 // Created by Jorge Jimenez on 6/11/25.
 //
 
-#ifndef VEC2_H
-#define VEC2_H
+#pragma once
 
+#include <SFML/Graphics.hpp>
+#include <cmath>
 
-
+template<typename T>
 class Vec2 {
+public:
+    T x = 0;
+    T y = 0;
 
+    /// Default constructor, sets x and y to 0
+    Vec2() = default;
+    /// Constructor with x and y values
+    /// \param xin The x value to set
+    /// \param yin The y value to set
+    Vec2(T xin, T yin)
+        : x(xin), y(yin) {}
+    // Constructor to convert sf::Vector2f to Vec2
+    Vec2(const sf::Vector2f vec)
+        : x(vec.x), y(vec.y) {}
+
+    // allow automatic conversion to sf::Vector2
+    // this lets us pass Vec2 in sfml fucntions
+    operator sf::Vector2<T>()
+    {
+        return sf::Vector2<T>(x, y);
+    }
+
+    Vec2 operator + (const Vec2& rhs) const
+    {
+        return Vec2(x + rhs.x, y + rhs.y);
+    }
+
+    Vec2 operator - (const Vec2& rhs) const
+    {
+        return Vec2(x - rhs.x, y - rhs.y);
+    }
+
+    Vec2 operator / (const T val) const
+    {
+        return Vec2(x / val, y / val);
+    }
+
+    Vec2 operator * (const T val) const
+    {
+        return Vec2(x * val, y * val);
+    }
+
+    bool operator == (const Vec2& rhs) const
+    {
+        return x == rhs.x && y == rhs.y;
+    }
+
+    bool operator != (const Vec2& rhs) const
+    {
+        return !(x == rhs.x && y == rhs.y);
+    }
+
+    void operator += (const Vec2& rhs)
+    {
+        x += rhs.x;
+        y += rhs.y;
+    }
+
+    void operator -= (const Vec2& rhs)
+    {
+        x -= rhs.x;
+        y -= rhs.y;
+    }
+
+    void operator *= (const T val)
+    {
+        x *= val;
+        y *= val;
+    }
+
+    void operator /= (const T val)
+    {
+        x /= val;
+        y /= val;
+    }
+
+    float dist(const Vec2& rhs) const
+    {
+        return sqrt((x - rhs.x) * (x - rhs.x) + (y - rhs.y) * (y - rhs.y));
+    }
 };
 
-
-
-#endif //VEC2_H
+using Vec2f = Vec2<float>;
